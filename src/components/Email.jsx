aa2862fb94c1065f130b2d480a632a6c74f2d93a
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-04-26 16:17:06 +0800

    added color

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -6,13 +6,13 @@ import { Button } from './ui/button';
 
 const Email = () => {
   return (
-    <section className={layout.section}>
+    <section className={`${layout.section} bg-gray-900 text-gray-200`}>
         <div className={layout.sectionInfo}>
             <div className='container mx-auto px-6 py-4 text-center'>
                 <span className={`${styles.heading}`}>Join with us, Subscribe our Newsletter</span>
                 <div className='flex items-center justify-center gap-2 container md:px-[15rem] md:py-4 px-0 py-0'>
                     <Input type='email' placeholder='Input your Email Address in here' className='py-3'/>
-                    <Button className='text-white py-3'>Subscribe</Button>
+                    <Button className='text-gray-900 py-3 bg-gray-300'>Subscribe</Button>
                 </div>
             </div>
         </div>
